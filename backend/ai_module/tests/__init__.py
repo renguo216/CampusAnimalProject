7ac195fd0a0a50abdc7b0953773b4269fde25582
jiujiujiujiujiu33 <3397628400@qq.com>
2026-05-30 16:22:21 +0800

    Delete backend/ai_module directory

--- backend/ai_module/tests/__init__.py
+++
@@ -1,5 +0,0 @@
-# 包标识文件
-path = r'D:\jupyter\CampusAnimalProject\backend\ai_module\tests\__init__.py'
-with open(path, 'w', encoding='utf-8') as f:
-    f.write('# tests包')
-print("tests/__init__.py 已创建")
